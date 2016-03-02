e81dcb6c10448c342e414bf662aac0c3e3b8e446
Bryan Hunt <bhunt@mac.com>
2016-03-02 08:54:32 -0600

    Make grid optional

--- tests/dummy/app/controllers/barchart.js
+++ tests/dummy/app/controllers/barchart.js
@@ -14,7 +14,6 @@ export default Controller.extend({
     title: { x: 250, y: 15, text: "Bar Chart", transform: "" },
     xAxis: { transform: "translate(35,455)", label: { x: 250, y: 495, text: "X Axis", transform: "" } },
     yAxis: { transform: "translate(35,25)", label: { x: -240, y: 10, text: "Y Axis", transform: "rotate(-90)" } },
-    xGrid: { transform: "translate(35,455)" },
     yGrid: { transform: "translate(35,25)" },
     plot: { transform: "translate(35,25)", paddingLeft: 35, paddingRight: 25, paddingTop: 25, paddingBottom: 45, showDots: true,
       barWidthTransform: function(dataPoint, scale) {return scale.rangeBand();}
@@ -26,7 +25,6 @@ export default Controller.extend({
   init() {
     this.set('chartOptions.xAxis.scale', this.get('xScale'));
     this.set('chartOptions.yAxis.scale', this.get('yScale'));
-    this.set('chartOptions.xGrid.scale', this.get('xScale'));
     this.set('chartOptions.yGrid.scale', this.get('yScale'));
     this.set('chartOptions.plot.xScale', this.get('xScale'));
     this.set('chartOptions.plot.yScale', this.get('yScale'));
