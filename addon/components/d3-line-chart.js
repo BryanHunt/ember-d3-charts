128d29621cc3563b028fb50e0b47faa0ab7b998f
Bryan Hunt <bhunt@mac.com>
2016-03-02 21:50:24 -0600

    Better initialization

--- addon/components/d3-line-chart.js
+++ addon/components/d3-line-chart.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
 import layout from '../templates/components/d3-line-chart';
+import Accessor from 'ember-d3-components/utils/accessor';
 
 export default Ember.Component.extend({
   layout,
+
+  init() {
+    this._super(...arguments);
+    this.xAccessor = this.xAccessor || Accessor.create({name: "x"});
+    this.yAccessor = this.yAccessor || Accessor.create({name: "y"});
+  },
 
   d3Transition(component, d3Data) {
     d3Data.transition();
